f4c9aa8f8993ebb681614ead216333b7e8cc799b
kawasaki kohei <kawasaki@ficc.jp>
2014-06-09 10:33:52 +0900

    first commit

---
+++ isie.js
@@ -0,0 +1,14 @@
+window.is_ie = 999;
+
+/*@cc_on
+@if (@_jscript_version == 10)
+    window.is_ie = 10;
+  @elif (@_jscript_version == 9)
+    window.is_ie = 9;
+  @elif (@_jscript_version == 5.8)
+    window.is_ie = 8;
+  @else
+    window.is_ie = 7;
+  @end
+@*/
+
